ec11abbe912a6b9ba8eaf712d9ca54c109910109
josephwilliams <josephwilli12@gmail.com>
2016-12-07 01:20:27 -0800

    react up

---
+++ components/splash.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Splash extends React.Component {
+  constructor () {
+    super();
+  }
+
+  render () {
+    return (
+      <div className="splash-container">
+        {'woah'}
+      </div>
+    );
+  }
+}
